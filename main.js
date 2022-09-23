c82a24a2c0c07cbc91bf43c9ecee196b307cb8e2
aksharaj1 <112727320+aksharaj1@users.noreply.github.com>
2022-09-22 17:48:24 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+// https://teachablemachine.withgoogle.com/models/gJsy03U4N/
+
+Webcam.attach('#camera')
+camera = document.getElementById("camera");
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:"png",
+    png_quality:90
+});
+
+function captureImg(){
+    Webcam.snap(function( data_uri) {
+        document.getElementById("result").innerHTML='<img src="'+data_uri+'" id="snapshot">'
+    });
+}
+
+
+
+
+
+
+
+
